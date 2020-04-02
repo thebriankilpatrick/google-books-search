f58763ae2b816848ef5ccc13a7f39c4225fed728
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-04-02 17:45:08 -0400

    Replaced files to fix dev server

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
+import Header from "./components/Header";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-  
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
 }
+
 
 export default App;
